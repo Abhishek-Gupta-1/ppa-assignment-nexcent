f8e26cd985e601da5b63cfcccc2ff98ef5ccc4c7
Abhishek Gupta <abhisheknew2023@gmail.com>
2024-07-06 12:40:27 +0530

    dark mode toggle added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { ModeToggle } from "@/components/ui/ModeToggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>This app is running </h1>
-      );
+    <div>
+      <ModeToggle />
+      <h1>This app is running </h1>
+    </div>
+
+  );
 }
